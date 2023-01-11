2d991f058c60f8444816299156ba3230d51c3428
Jhon <jhon@visagov.com>
2023-01-11 23:20:12 +0100

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,8 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+import ResumeForm from './components/resumes/ResumeForm.vue';
+app.component('resume-form', ResumeForm);
 
 /**
  * The following block of code may be used to automatically register your
---
+++ resources/js/components/resumes/ResumeForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Resume components</div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log('resume components')
+    }
+}
+</script>
