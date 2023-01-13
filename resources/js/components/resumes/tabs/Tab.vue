5a04b951ce020772c835a9f10db135ab5baafa26
Jhon <jhon@visagov.com>
2023-01-13 22:58:22 +0100

    add vue3-form-generator bootstrap-vue fortawesome

---
+++ resources/js/components/resumes/tabs/Tab.vue
@@ -0,0 +1,35 @@
+<template>
+  <BTab title-link-class="p-2">
+    <template #title>
+      <div class="d-flex justify-content-center">
+        <div>
+          <i :class="icon"></i>
+        </div>
+        <div>
+          {{ title }}
+        </div>
+      </div>
+    </template>
+    <slot></slot>
+  </BTab>
+</template>
+
+<script>
+import BTab from 'bootstrap-vue';
+export default {
+  name: 'Tab',
+
+  props: {
+    title: {
+      type: String,
+    },
+    icon: {
+      type: String,
+    },
+  },
+
+  components: {
+    BTab,
+  },
+};
+</script>
--- resources/js/components/resumes/tabs/Tabs.vue
+++ resources/js/components/resumes/tabs/Tabs.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-  </div>
+  
 </template>
 
 <script>
-
 export default {
-  name: 'ResumeForm',
 
-};
+}
 </script>
+
+<style>
+
+</style>
